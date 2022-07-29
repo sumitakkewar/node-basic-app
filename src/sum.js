4c287b16a30d65a8aba279ad889173b33952eb7a
Sumit Akkewar <sumitakkewar@gmail.com>
2022-07-30 02:59:27 +0530

    made changes for div function

--- src/sum.js
+++ src/sum.js
@@ -5,3 +5,7 @@ export default function sum(a, b) {
 export function sub(a, b) {
     return a - b;
 }
+
+export function div(a, b) {
+    return a / b;
+}
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,9 +1,13 @@
-import sum, { sub }  from '../src/sum'
+import sum, { div, sub }  from '../src/sum'
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 2 - 1 to equal 1', () => {
+test('sub 2 - 1 to equal 1', () => {
     expect(sub(2, 1)).toBe(1);
+});
+
+test('div 4 / 2 to equal 2', () => {
+    expect(div(4, 2)).toBe(2);
 });
