419ddeb215ee85eb148e7f0bdf9ad9a2edaa49a9
Sumit Akkewar <sumitakkewar@gmail.com>
2022-07-30 02:45:18 +0530

    made changes for sub functions

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,7 @@
 export default function sum(a, b) {
     return a + b;
 }
+
+export function sub(a, b) {
+    return a - b;
+}
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,9 @@
-import sum  from '../src/sum'
+import sum, { sub }  from '../src/sum'
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 2 - 1 to equal 1', () => {
+    expect(sub(2, 1)).toBe(1);
 });
