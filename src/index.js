3cb33859edfc1509f924003f5b7c173a16c7a180
Sumit Akkewar <sumit@creditap.ai>
2022-07-29 22:02:00 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 1234;
+
+app.get('/', (req, res) => {
+    res.status(200).send(`Hello ${process.env.NODE_ENV}`)
+})
+
+app.listen(port, () => {
+    console.log(`App is listening on: ${port}`)
+})
